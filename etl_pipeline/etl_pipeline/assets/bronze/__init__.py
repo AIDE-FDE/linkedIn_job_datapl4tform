ba8e711f303662c1cc3a7b0653ee5101d21d6809
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-22 16:08:26 +0700

    bronze layer version 1

--- etl_pipeline/etl_pipeline/assets/bronze/__init__.py
+++ etl_pipeline/etl_pipeline/assets/bronze/__init__.py
@@ -9,5 +9,5 @@ bronze_assets = load_assets_from_modules([
     bronze_linkedIn_companies,
     bronze_linkedIn_jobs,
     bronze_linkedIn_mapping,
-    # bronze_linkedIn_postings,
+    bronze_linkedIn_postings,
 ])
--- etl_pipeline/etl_pipeline/assets/bronze/bronze_linkedIn_companies.py
+++ etl_pipeline/etl_pipeline/assets/bronze/bronze_linkedIn_companies.py
@@ -4,7 +4,8 @@
 # employee_counts 
 
 import pandas as pd
-from dagster import asset, Output
+from dagster import asset, Output, DailyPartitionsDefinition
+import datetime
 
 
 Tables = [
@@ -42,3 +43,43 @@ def create_bronze_asset (table_name: str):
 bronze_companies_companies = create_bronze_asset ("companies")
 bronze_companies_company_industries = create_bronze_asset ("company_industries")
 bronze_companies_company_specialities = create_bronze_asset ("company_specialities")
+
+
+
+table_partition = "employee_counts"
+
+@asset(
+    name=f"bronze_companies_{table_partition}",
+    key_prefix=["bronze", "linkedin", "companies"],
+    io_manager_key="minio_io_manager",
+    required_resource_keys={"mysql_io_manager"},
+    compute_kind="MySQL",
+    partitions_def=DailyPartitionsDefinition(start_date="2023-01-01"),
+    group_name="bronze"
+)
+def bronze_asset(context) -> Output[pd.DataFrame]:
+    try:
+        partition_date_str = context.asset_partition_key_for_output()
+        partition_date = datetime.datetime.strptime(partition_date_str, "%Y-%m-%d")
+
+        start_ts = int(partition_date.timestamp())
+        end_ts = int((partition_date + datetime.timedelta(days=1)).timestamp()) - 1
+
+        sql_statement = f"""
+            SELECT *
+            FROM {table_partition}
+            WHERE time_recorded BETWEEN {start_ts} AND {end_ts}
+        """
+        context.log.info(f"Running partitioned query for date: {partition_date_str}")
+    except Exception:
+        context.log.info(f"{table_partition} has no partition key! Loading full table.")
+        sql_statement = f"SELECT * FROM {table_partition}"
+
+    pd_data = context.resources.mysql_io_manager.extract_data(sql_statement)
+    context.log.info(pd_data.head(10))
+
+    return Output(
+        pd_data,
+        metadata={"table": table_partition, "record_count": len(pd_data)}
+    )
+
