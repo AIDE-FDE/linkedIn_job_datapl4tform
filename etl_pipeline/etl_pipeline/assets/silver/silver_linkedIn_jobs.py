90f8de2cd0e5be71d4722d547942930a84601d40
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-27 16:14:59 +0700

    silver layer: job description table

--- etl_pipeline/etl_pipeline/assets/silver/silver_linkedIn_jobs.py
+++ etl_pipeline/etl_pipeline/assets/silver/silver_linkedIn_jobs.py
@@ -1,4 +1,3 @@
-from doctest import master
 from dagster import asset, Output, AssetIn, AssetKey, AssetExecutionContext
 import pandas as pd
 
@@ -17,7 +16,7 @@ from pyspark.sql import SparkSession
     group_name='silver',
     compute_kind='Spark'
 )
-def silver_job_skills_asset (
+def silver_jobs_job_skills (
     context: AssetExecutionContext,
     bronze_jobs_job_skills: pd.DataFrame
 ) -> Output[pd.DataFrame]:
@@ -45,14 +44,96 @@ def silver_job_skills_asset (
         pandas_df,
         metadata={
             'table': 'silver_jobs_job_skills',
-            'record': len (pandas_df)
+            'record': len (pandas_df),
+            'column': list (pandas_df.columns)
         }
     )
 
 
+# benefits          
+@asset (
+    ins= {
+        'bronze_jobs_benefits': AssetIn (key_prefix=["bronze", "linkedin", "jobs"])
+    },
+    name='silver_jobs_benefits',
+    io_manager_key='minio_io_manager',
+    key_prefix=["silver", "linkedin", "jobs"],
+    group_name='silver',
+    compute_kind='pandas'
+)
+def silver_jobs_benefits (
+    context: AssetExecutionContext,
+    bronze_jobs_benefits: pd.DataFrame
+) -> Output[pd.DataFrame]:
+    pandas_df = bronze_jobs_benefits
 
-# benefits             
-# industries           
+    context.log.info (f'clean {len (pandas_df)} records to silver layer')
+
+    return Output (
+        pandas_df,
+        metadata={
+            'table': 'silver_jobs_benefits',
+            'record': len (pandas_df),
+            'column': list (pandas_df.columns)
+        }
+    )   
+
+
+
+# industries
+@asset (
+    ins= {
+        'bronze_jobs_industries': AssetIn (key_prefix=["bronze", "linkedin", "jobs"])
+    },
+    name='silver_jobs_industries',
+    io_manager_key='minio_io_manager',
+    key_prefix=["silver", "linkedin", "jobs"],
+    group_name='silver',
+    compute_kind='Spark'
+)
+def silver_jobs_industries (
+    context: AssetExecutionContext,
+    bronze_jobs_industries: pd.DataFrame
+) -> Output[pd.DataFrame]:
+    spark = (
+        SparkSession.builder.appName ('silver_jobs_industries')
+                            .master ("spark://spark-master:7077")
+                            .getOrCreate ()
+    )
+
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+    spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "true")
+
+    bronze_jobs_industries_df = spark.createDataFrame (bronze_jobs_industries)
+    bronze_jobs_industries_df.createOrReplaceTempView ('industries')
+
+
+    sql_stm = """
+        SELECT
+            industry_id,
+            COALESCE(
+                NULLIF(REGEXP_EXTRACT(industry_name, '"([^"]+)"', 1), ''), 
+                industry_name
+            ) AS industry_name
+        FROM industries
+    """
+
+    cleaned_df = spark.sql (sql_stm)
+
+    pandas_df = cleaned_df.toPandas ()
+
+    context.log.info (f'clean {len (pandas_df)} records to silver layer')
+
+    return Output (
+        pandas_df,
+        metadata={
+            'table': 'silver_jobs_industries',
+            'record': len (pandas_df),
+            'column': list (pandas_df.columns)
+        }
+    )      
+
+
 # job_industries       
 @asset (
     ins= {
@@ -64,18 +145,19 @@ def silver_job_skills_asset (
     group_name='silver',
     compute_kind='pandas'
 )
-def silver_job_industries_asset (
+def silver_jobs_job_industries (
     context: AssetExecutionContext,
     bronze_jobs_job_industries: pd.DataFrame
 ) -> Output[pd.DataFrame]:
-    pandas_df = bronze_jobs_job_industries.copy ()
+    pandas_df = bronze_jobs_job_industries
 
     context.log.info (f'clean {len (pandas_df)} records to silver layer')
 
     return Output (
         pandas_df,
         metadata={
-            'table': 'silver_jobs_job_skills',
-            'record': len (pandas_df)
+            'table': 'silver_jobs_job_industries',
+            'record': len (pandas_df),
+            'column': list (pandas_df.columns)
         }
     )
