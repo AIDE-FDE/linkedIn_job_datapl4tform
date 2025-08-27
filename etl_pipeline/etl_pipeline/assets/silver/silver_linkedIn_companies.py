bf271abb593c872ed1babe096f1484cbce9e6a18
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-28 00:45:00 +0700

    silver company

--- etl_pipeline/etl_pipeline/assets/silver/silver_linkedIn_companies.py
+++ etl_pipeline/etl_pipeline/assets/silver/silver_linkedIn_companies.py
@@ -1,4 +1,4 @@
-from dagster import asset, Output, AssetIn, AssetKey, AssetExecutionContext
+from dagster import asset, Output, AssetIn, AssetKey, AssetExecutionContext, DailyPartitionsDefinition
 import pandas as pd
 
 from pyspark.sql.functions import regexp_replace, regexp_extract, when, col
@@ -66,5 +66,125 @@ def silver_companies_company_industries (
             'column': list (pandas_df.columns)
         }
     )  
-# company_specialities 
+
+
+
+# company_specialities
+@asset (
+    ins= {
+        'bronze_companies_company_specialities': AssetIn (key_prefix=["bronze", "linkedin", "companies"])
+    },
+    name='silver_companies_company_specialities',
+    io_manager_key='minio_io_manager',
+    key_prefix=["silver", "linkedin", "companies"],
+    group_name='silver',
+    compute_kind='Spark'
+)
+def silver_companies_company_specialities (
+    context: AssetExecutionContext,
+    bronze_companies_company_specialities: pd.DataFrame
+) -> Output[pd.DataFrame]:
+    spark = (
+        SparkSession.builder.appName ('silver_companies_company_specialities')
+                            .master ("spark://spark-master:7077")
+                            .getOrCreate ()
+    )
+
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+    spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "true")
+
+    bronze_companies_company_specialities_df = spark.createDataFrame (bronze_companies_company_specialities)
+    bronze_companies_company_specialities_df.createOrReplaceTempView ('company_specialities')
+
+    sql_stm = """
+        SELECT
+            company_id,
+            CASE
+                WHEN REGEXP_EXTRACT(speciality, '"([^"]+)"', 1) != '' 
+                THEN REGEXP_EXTRACT(speciality, '"([^"]+)"', 1)
+                ELSE speciality
+            END AS speciality
+        FROM company_specialities
+    """
+
+    cleaned_df = spark.sql (sql_stm)
+
+    context.log.info(f"Spark count before remove duplicate: {cleaned_df.count()}")
+
+    cleaned_df = cleaned_df.dropDuplicates(list (bronze_companies_company_specialities.columns))
+
+    context.log.info(f"Spark count before toPandas: {cleaned_df.count()}")
+
+    pandas_df = cleaned_df.toPandas()
+    context.log.info(f"Pandas shape: {pandas_df.shape}")
+
+
+    context.log.info (f'clean {len (pandas_df)} records to silver layer')
+
+    return Output (
+        pandas_df,
+        metadata={
+            'table': 'company_industries',
+            'record': len (pandas_df),
+            'column': list (pandas_df.columns)
+        }
+    )   
+
+
 # employee_counts 
+@asset(
+    ins={
+        "bronze_companies_employee_counts": AssetIn(
+            key_prefix=["bronze", "linkedin", "companies"]
+        )
+    },
+    name="silver_companies_employee_counts",
+    io_manager_key="minio_io_manager",
+    key_prefix=["silver", "linkedin", "companies"],
+    group_name="silver",
+    partitions_def=DailyPartitionsDefinition(start_date="2023-01-01"),
+    compute_kind="Spark",
+)
+def silver_companies_employee_counts(
+    context: AssetExecutionContext,
+    bronze_companies_employee_counts: pd.DataFrame,
+) -> Output[pd.DataFrame]:
+    spark = (
+        SparkSession.builder.appName("silver_companies_employee_counts")
+                            .master("spark://spark-master:7077")
+                            .getOrCreate()
+    )
+
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+    spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "true")
+
+    bronze_df = spark.createDataFrame(bronze_companies_employee_counts)
+    bronze_df.createOrReplaceTempView("employee_counts")
+
+    sql_stm = """
+        SELECT
+            company_id, employee_count, follower_count, time_recorded
+        FROM employee_counts
+    """
+
+    cleaned_df = spark.sql(sql_stm)
+
+    cleaned_df = cleaned_df.dropDuplicates(
+        ["company_id", "time_recorded", "employee_count", "employee_count_range"]
+    )
+
+    context.log.info(f"Spark count before toPandas: {cleaned_df.count()}")
+
+    pandas_df = cleaned_df.toPandas()
+    context.log.info(f"Pandas shape: {pandas_df.shape}")
+
+    context.log.info(f"Cleaned {len(pandas_df)} records to silver layer")
+
+    return Output(
+        pandas_df,
+        metadata={
+            "table": "employee_counts",
+            "record": len(pandas_df),
+            "column": list(pandas_df.columns),
+        },
+    )
