eeb1b006311f5adab408acc57ed062a3f3b557a7
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-29 14:47:23 +0700

    dimension tables

---
+++ etl_pipeline/etl_pipeline/assets/gold/fact/fact_gold_postings.py
@@ -0,0 +1,35 @@
+from dagster import asset, Output, AssetIn, AssetKey, AssetExecutionContext, DailyPartitionsDefinition, WeeklyPartitionsDefinition
+import pandas as pd
+
+from pyspark.sql.functions import regexp_replace, regexp_extract, when, col, lit
+from pyspark.sql import SparkSession
+
+@asset (
+    ins= {
+        'silver_companies_companies': AssetIn (key_prefix=["silver", "linkedin", "companies"])
+    },
+
+    name='fact_gold_companies',
+    io_manager_key="minio_io_manager",
+    key_prefix=["gold", "linkedin", 'fact'],
+    group_name="gold",
+    compute_kind="pandas",
+
+)
+def fact_gold_companies (
+    context: AssetExecutionContext,
+    silver_companies_companies: pd.DataFrame
+) -> Output[pd.DataFrame]:
+
+    context.log.info (silver_companies_companies.head (10))
+    total_record = len (silver_companies_companies)
+    context.log.info (f'Join total {total_record} to fact_gold_companies')
+
+    return Output (
+        silver_companies_companies,
+        metadata={
+            "table": "fact_gold_companies",
+            'record': total_record,
+            'columns': list (silver_companies_companies.columns),
+        }
+    )
