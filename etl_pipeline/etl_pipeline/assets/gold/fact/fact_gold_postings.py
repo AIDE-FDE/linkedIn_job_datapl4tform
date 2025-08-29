ab160be6a4d9f2882d98fcdc4ae762c5528ea466
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-29 17:09:37 +0700

    fact: postings

--- etl_pipeline/etl_pipeline/assets/gold/fact/fact_gold_postings.py
+++ etl_pipeline/etl_pipeline/assets/gold/fact/fact_gold_postings.py
@@ -6,30 +6,44 @@ from pyspark.sql import SparkSession
 
 @asset (
     ins= {
-        'silver_companies_companies': AssetIn (key_prefix=["silver", "linkedin", "companies"])
+        'silver_postings_postings': AssetIn (key_prefix=["silver", "linkedin", "postings"])
     },
 
-    name='fact_gold_companies',
+    name='fact_gold_postings',
     io_manager_key="minio_io_manager",
     key_prefix=["gold", "linkedin", 'fact'],
+    partitions_def=WeeklyPartitionsDefinition(start_date="2023-01-01", day_offset=1),  
     group_name="gold",
-    compute_kind="pandas",
+    compute_kind="Spark",
 
 )
-def fact_gold_companies (
+def fact_gold_postings (
     context: AssetExecutionContext,
-    silver_companies_companies: pd.DataFrame
+    silver_postings_postings: pd.DataFrame
 ) -> Output[pd.DataFrame]:
+    spark = (
+        SparkSession.builder.appName("fact_gold_postings")
+                            .master("spark://spark-master:7077")
+                            .getOrCreate()
+    )
 
-    context.log.info (silver_companies_companies.head (10))
-    total_record = len (silver_companies_companies)
-    context.log.info (f'Join total {total_record} to fact_gold_companies')
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+    spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "true")
+
+    df_postings = spark.createDataFrame (silver_postings_postings)
+    df_postings = df_postings.drop ('job_id', 'max_salary', 'med_salary', 'min_salary', 'pay_period', 'currency', 'compensation_type')
+    
+    result = df_postings.toPandas ()
+
+    context.log.info (result.head (10))
+    total_record = len (result)
+    context.log.info (f'Join total {total_record} to fact_gold_postings')
 
     return Output (
-        silver_companies_companies,
+        result,
         metadata={
-            "table": "fact_gold_companies",
+            "table": "fact_gold_postings",
             'record': total_record,
-            'columns': list (silver_companies_companies.columns),
+            'columns': list (result.columns),
         }
     )
