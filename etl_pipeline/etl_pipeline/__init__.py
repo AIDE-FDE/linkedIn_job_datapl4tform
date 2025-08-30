687468d617ede797bc131b4cf92c8ad65e95ba5d
mnhtndev <tuannguyen.02042005@gmail.com>
2025-08-30 21:29:24 +0700

    dbt assets

--- etl_pipeline/etl_pipeline/__init__.py
+++ etl_pipeline/etl_pipeline/__init__.py
@@ -8,6 +8,12 @@ from etl_pipeline.config.io_manager_config import *
 from etl_pipeline.resources.minio_io_manager import MinIOIOManager
 from etl_pipeline.resources.psql_io_manager import PostgreSQLIOManager
 from etl_pipeline.resources.mysql_io_manager import MySQLIOManager
+from dagster_dbt import DbtCliResource
+
+
+from .constants import dbt_manifest_path, dbt_project_dir, dbt_profile_dir
+
+import os
 
 all_assets = load_assets_from_modules([assets])
 
@@ -19,6 +25,7 @@ defs = Definitions(
         'minio_io_manager': MinIOIOManager(MINIO_CONFIG),
         'psql_io_manager': PostgreSQLIOManager (PSQL_CONFIG),
         # 'clickhouse_io_manager': ClickHouseIOManager(CLICKHOUSE_CONFIG),
+         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir), profiles_dir=os.fspath(dbt_profile_dir))
     }
 )
 
